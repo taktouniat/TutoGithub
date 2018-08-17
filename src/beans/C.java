26864f4d0b1eed5b1ff8a49cd02ebafd41e810c5
taktouniat <taktouniat@yahoo.fr>
2018-08-17 13:29:44 +0200

    ajout de la classe C

---
+++ src/beans/C.java
@@ -0,0 +1,7 @@
+package beans;
+
+public class C {
+	public C() {
+		System.out.println("Bonjour de la classe C");
+	}
+}
--- src/main/Test.java
+++ src/main/Test.java
@@ -2,12 +2,14 @@ package main;
 
 import beans.A;
 import beans.B;
+import beans.C;
 
 public class Test {
 
 	public static void main(String[] args) {
 		A a = new A();
 		B b = new B();
+		C c = new C();
 
 	}
 
